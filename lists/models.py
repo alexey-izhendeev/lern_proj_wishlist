29a82af27fabc8b5aad0b6414967fb44127a78d4
Alexey Izhendeev <takedo19@gmail.com>
2021-03-29 16:26:21 +0700

    Have made a new branch, deleted garbage, added comments.

--- lists/models.py
+++ lists/models.py
@@ -9,7 +9,6 @@ class WishListItem(models.Model):
     note = models.CharField(max_length=500)
     userid = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
 
-
     def __str__(self):
         return self.name
 
--- lists/views.py
+++ lists/views.py
@@ -5,11 +5,9 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.shortcuts import redirect
-from django.contrib.auth.decorators import login_required
 
 
-def index(request):
+def index(request):  # main page
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
     else:
@@ -18,7 +16,7 @@ def index(request):
     })
 
 
-def user_wishes(request, user_id):
+def user_wishes(request, user_id):  # all items of wishlist
     wishlist = WishListItem.objects.all().filter(userid=user_id)
     user = User.objects.get(pk=user_id)
     return render(request, 'lists/user.html', {
@@ -27,7 +25,7 @@ def user_wishes(request, user_id):
     })
 
 
-def add_item(request, user_id):
+def add_item(request, user_id):  # adds an item to wishlist
     if request.method == 'POST':
         form = WishForm(request.POST)
         if form.is_valid():
@@ -42,37 +40,32 @@ def add_item(request, user_id):
         })
 
 
-def delete_item(request, user_id):
+def delete_item(request, user_id):  # delits an item from wishlist
     if request.method == 'POST':
         item = WishListItem.objects.get(pk=int(request.POST['item']))
         item.delete()
         return HttpResponseRedirect(reverse('user', args=[user_id]))
 
 
-def edit_item(request, user_id):
+def edit_item(request, user_id):  # redirects to a page, where item can be edited
     if request.method == 'POST':
         item = WishListItem.objects.get(pk=int(request.POST['item']))
 
         return render(request, 'lists/edit_item.html', {
             'item': item,
         })
-        #return HttpResponseRedirect(reverse('add', args=[user_id]))
 
 
-def save_item(request, user_id, item_id):
+def save_item(request, user_id, item_id):  # when item is edited(updated) it saves the item
     if request.method == 'POST':
-        # wishlist = WishListItem.objects.all().filter(userid=user_id)
         item = WishListItem.objects.get(pk=item_id)
-        # item = WishListItem.objects.get(pk=int(request.POST['item']))
         item.name = request.POST['name']
         item.price = request.POST['price']
         item.link = request.POST['link']
         item.note = request.POST['note']
-        #item.userid = user_id
         item.save()
 
         return HttpResponseRedirect(reverse('user', args=[user_id]))
-
 
 
 def login_view(request):
