aed2034cc29bb065f1ebfd0e660db2328e9b984f
Alexey Izhendeev <takedo19@gmail.com>
2021-04-20 14:34:19 +0700

    Have made api urls and views

--- lists/api/urls.py
+++ lists/api/urls.py
@@ -7,4 +7,5 @@ app_name = 'lists'
 urlpatterns = [
     path('wishlist/', views.WishlistListView.as_view(), name='wishlist_list'),
     path('wishlist/<pk>/', views.WishlistDetailView.as_view(), name='wishlist_detail'),
+    path('wishlist/<pk>/view', views.DeleteItem.as_view(), name='view_item'),
 ]
--- lists/api/views.py
+++ lists/api/views.py
@@ -2,7 +2,6 @@ from rest_framework import generics
 from ..models import WishListItem
 from .serializers import WishListSerializer
 
-
 class WishlistListView(generics.ListAPIView):
 
     queryset = WishListItem.objects.all()
